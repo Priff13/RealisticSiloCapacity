b75a84badcbd0db233d78bc35ffde793fe940133
Becket Evans <becketevans09@gmail.com>
2024-12-20 15:47:12 -0700

    Add project files.

---
+++ RealisticSiloCapacity/ModEntry.cs
@@ -0,0 +1,37 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley.GameData.Buildings;
+namespace RealisticSiloCapacity
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper? helper)
+        {
+            if (helper == null)
+            {
+                return;
+            }
+            helper.Events.Content.AssetRequested += this.OnAssetRequested;
+        }
+
+        private void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
+        {
+            if (!e.NameWithoutLocale.IsEquivalentTo("Data/Buildings"))
+            {
+                return;
+            }
+
+            e.Edit(asset =>
+            {
+                IDictionary<string, BuildingData> data = asset.AsDictionary<string, BuildingData>().Data;
+                if (data.TryGetValue("Silo", out BuildingData? silodata))
+                {
+                    if (silodata != null)
+                    {
+                        silodata.HayCapacity = 325000;
+                    }
+                }
+            });
+        }
+    }
+}
